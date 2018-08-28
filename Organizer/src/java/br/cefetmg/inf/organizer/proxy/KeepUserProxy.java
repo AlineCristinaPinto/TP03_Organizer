697d152cab7e4afb4c10b7aa5ce45c0b83c6c123
Eduardo Cotta <dudufrancamg@gmail.com>
2018-08-28 10:31:34 -0300

    Adicionando Proxy de User

--- Organizer/src/java/br/cefetmg/inf/organizer/proxy/KeepUserProxy.java
+++ Organizer/src/java/br/cefetmg/inf/organizer/proxy/KeepUserProxy.java
@@ -8,7 +8,9 @@ import br.cefetmg.inf.util.RequestType;
 import br.cefetmg.inf.util.exception.BusinessException;
 import br.cefetmg.inf.util.exception.PersistenceException;
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -49,22 +51,102 @@ public class KeepUserProxy implements IKeepUser {
 
     @Override
     public User searchUser(User user) throws PersistenceException, BusinessException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PseudoPackage contentPackage;
+        Gson json = new Gson();
+        User returnUser = null;
+                
+        List<String> jsonContent;
+        jsonContent = new ArrayList();
+        jsonContent.add(json.toJson(user));
+        
+        RequestType requestType = RequestType.SEARCHUSER;
+        contentPackage = new PseudoPackage(requestType, jsonContent);
+        try {
+            PseudoPackage receivedPackage = client.request(contentPackage);
+            
+            JsonReader reader = new JsonReader(new StringReader(receivedPackage.getContent().get(0)));
+            reader.setLenient(true);
+            
+            returnUser = json.fromJson(reader, User.class);
+            return returnUser;
+            
+        } catch (IOException ex) {
+            Logger.getLogger(KeepUserProxy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return returnUser;
     }
 
     @Override
     public boolean updateUser(User user) throws PersistenceException, BusinessException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PseudoPackage contentPackage;
+        Gson json = new Gson();
+        
+        List<String> jsonContent;
+        jsonContent = new ArrayList();
+        jsonContent.add(json.toJson(user));
+        
+        RequestType requestType = RequestType.UPDATEUSER;
+        contentPackage = new PseudoPackage(requestType, jsonContent);
+        
+        try {
+            PseudoPackage receivedPackage = client.request(contentPackage);
+          
+            return Boolean.valueOf(receivedPackage.getContent().get(0));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(KeepUserProxy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     @Override
     public boolean deleteAccount(User user) throws PersistenceException, BusinessException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PseudoPackage contentPackage;
+        Gson json = new Gson();
+        
+        List<String> jsonContent;
+        jsonContent = new ArrayList();
+        jsonContent.add(json.toJson(user));
+        
+        RequestType requestType = RequestType.DELETEACCOUNT;
+        contentPackage = new PseudoPackage(requestType, jsonContent);
+        try {
+            PseudoPackage receivedPackage = client.request(contentPackage);
+          
+            return Boolean.valueOf(receivedPackage.getContent().get(0));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(KeepUserProxy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     @Override
     public User getUserLogin(String email, String password) throws PersistenceException, BusinessException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PseudoPackage contentPackage;
+        Gson json = new Gson();
+        User returnUser = null;
+                
+        List<String> jsonContent;
+        jsonContent = new ArrayList();
+        jsonContent.add(email);
+        jsonContent.add(password);
+        
+        RequestType requestType = RequestType.GETUSERLOGIN;
+        contentPackage = new PseudoPackage(requestType, jsonContent);
+        try {
+            PseudoPackage receivedPackage = client.request(contentPackage);
+            
+            JsonReader reader = new JsonReader(new StringReader(receivedPackage.getContent().get(0)));
+            reader.setLenient(true);
+            
+            returnUser = json.fromJson(reader, User.class);
+            return returnUser;
+            
+        } catch (IOException ex) {
+            Logger.getLogger(KeepUserProxy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return returnUser;
     }
 
 }
